c001970f409c533b0ac8bb8b1db576bc16617f70
Kamila3361 <kamila.tashimova@nu.edu.kz>
2025-07-16 09:11:10 +0500

    all

--- config/database.py
+++ config/database.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
-uri = "mongodb+srv://tasimovakamila:EGD0ds2vVQoiDLdZ@cluster0.o3tzhxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+uri = os.getenv("DATABASE_URL")
 
 client = MongoClient(uri)
 
